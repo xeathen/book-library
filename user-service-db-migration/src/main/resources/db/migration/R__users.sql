0589cf739cb10d8056f1a32f75a903c0f2a9a7f2
xeathen <x929774062@gmail.com>
2019-12-25 19:44:58 +0800

    optimize sql

--- user-service-db-migration/src/main/resources/db/migration/R__users.sql
+++ user-service-db-migration/src/main/resources/db/migration/R__users.sql
@@ -7,11 +7,11 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users`
 (
-    `id`         bigint(11)                                                   NOT NULL AUTO_INCREMENT,
-    `user_name`  varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-    `password`   varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-    `user_email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-    `status`     bit(1)                                                       NULL DEFAULT b'1',
+    `id`         bigint(11)   NOT NULL  AUTO_INCREMENT,
+    `user_name`  varchar(50)  NOT NULL,
+    `password`   varchar(50)  NOT NULL,
+    `user_email` varchar(50)  NOT NULL,
+    `status`     bit(1)       NULL      DEFAULT b'1',
     PRIMARY KEY (`id`) USING BTREE,
     UNIQUE INDEX `user_name` (`user_name`, `user_email`) USING BTREE
 ) ENGINE = InnoDB
