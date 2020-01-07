c05c379963c72d412d2e9fb1bbf7634b20805845
ethan <x929774062@gmail.com>
2020-01-07 14:13:35 +0800

    Optimize imports.

--- book-service/src/main/java/app/book/service/BOBookService.java
+++ book-service/src/main/java/app/book/service/BOBookService.java
@@ -28,7 +28,6 @@ import core.framework.web.exception.NotFoundException;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 /**
